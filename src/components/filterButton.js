a248adf49ba70785f6de1cbda948e5d1e8687a4b
maheswari23 <maheswari.ksubburaj@gmail.com>
2021-10-12 21:06:43 -0600

    Key added in filterbutton.

--- src/components/filterButton.js
+++ src/components/filterButton.js
@@ -1,9 +1,8 @@
 import { React } from 'react';
 import context from '../core/context';
 
-const filterButton = (filter) =>
-
+const filterButton = (filter) => <div key={ filter }>
 	<button onClick={ () => context.actions.setFilter(filter) }>
-		{filter}</button>;
+		{filter}</button></div>;
 
 export default filterButton;
