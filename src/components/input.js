2305d86c353a3bd1d60aeadc2487d36769aca0ed
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-10-15 14:00:44 +0000

    Actions key added.

--- src/components/input.js
+++ src/components/input.js
@@ -1,14 +1,22 @@
+/* eslint-disable no-console */
 import { React } from 'react';
 import context from '../core/context.js';
 
+const actionKeys = {
+	Enter: () => context.actions.addTodo(),
+	Escape: () => context.actions.setInput(''),
+};
+
 const TextBox = () =>
-	<div className="input">
-		<input
-			value={ context.state.input }
-			onChange={ (evt) =>
-				context.actions.setInput(evt.target.value) }
-			type="text"
-		/>
-	</div>;
+
+	<input
+		value={ context.state.input }
+		onChange={ (evt) =>
+			context.actions.setInput(evt.target.value) }
+		onKeyUp={ (evt) => {
+			console.log(actionKeys[evt.code]);
+			actionKeys[evt.code] && actionKeys[evt.code]();
+		} }
+	/>;
 
 export default TextBox;
