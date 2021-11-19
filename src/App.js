fa513b80152bdf67e650d508d8bccf6e86cb7913
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-11-19 13:02:45 -0700

    Added newtask and ticker

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.scss';
 import TodoPane from './components';
 import TaskPane from './components/taskPane';
 import TaskManager from './services/taskManager';
+import Ticker from './services/ticker';
 
 const App = () => {
 	useEffect(TaskManager.init, []);
+	useEffect(Ticker.start, []);
 
 	return (
 		<div className="App">
--- src/core/config.js
+++ src/core/config.js
@@ -1,8 +1,12 @@
+const milliseconds = 1000;
+const seconds = 5;
+
 const config = {
 	countStart: 0,
 	increment: 1,
 	rndStringLength: 8,
 	idLength: 16,
+	tickerDelay: milliseconds * seconds,
 };
 
 export default config;
